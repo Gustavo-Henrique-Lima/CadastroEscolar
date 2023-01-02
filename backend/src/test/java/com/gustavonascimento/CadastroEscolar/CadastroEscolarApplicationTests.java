cbd4611163630a2aa2c8f7cc3c1c381128c138c3
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-02 10:49:35 -0300

    Criação do backend

---
+++ backend/src/test/java/com/gustavonascimento/CadastroEscolar/CadastroEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.CadastroEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
