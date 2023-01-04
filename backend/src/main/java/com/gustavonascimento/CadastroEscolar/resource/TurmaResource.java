6c80544fb90201e562d83f70770e707910771f35
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-01-04 11:18:59 -0300

    Criação do CRUD da classe Turma

--- backend/src/main/java/com/gustavonascimento/CadastroEscolar/resource/TurmaResource.java
+++ backend/src/main/java/com/gustavonascimento/CadastroEscolar/resource/TurmaResource.java
@@ -1,12 +1,21 @@
 package com.gustavonascimento.CadastroEscolar.resource;
 
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gustavonascimento.CadastroEscolar.entitys.Turma;
 import com.gustavonascimento.CadastroEscolar.service.TurmaService;
@@ -23,5 +32,34 @@ public class TurmaResource {
 	{
 		List<Turma> turmas=servTur.findall();
 		return ResponseEntity.ok(turmas);
+	}
+	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Turma> findById(@PathVariable Long id)
+	{
+		Turma turma=servTur.findById(id);
+		return ResponseEntity.ok(turma);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Turma> insert(@Valid @RequestBody Turma turma)
+	{
+		turma=servTur.insert(turma);
+		URI uri=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(turma.getId()).toUri();
+		return ResponseEntity.created(uri).body(turma);
+	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Turma> update(@PathVariable Long id,@RequestBody Turma turma)
+	{
+		turma=servTur.update(id, turma);
+		return ResponseEntity.ok(turma);
+	}
+	
+	@DeleteMapping(value= "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id)
+	{
+		servTur.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- backend/src/main/java/com/gustavonascimento/CadastroEscolar/service/TurmaService.java
+++ backend/src/main/java/com/gustavonascimento/CadastroEscolar/service/TurmaService.java
@@ -1,12 +1,18 @@
 package com.gustavonascimento.CadastroEscolar.service;
 
 import java.util.List;
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.gustavonascimento.CadastroEscolar.entitys.Turma;
 import com.gustavonascimento.CadastroEscolar.repository.TurmaRepository;
+import com.gustavonascimento.CadastroEscolar.service.exceptions.DatabaseException;
+import com.gustavonascimento.CadastroEscolar.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class TurmaService {
@@ -17,5 +23,53 @@ public class TurmaService {
 	public List<Turma> findall()
 	{
 		return repoTur.findAll();
+	}
+	
+	public Turma findById(Long id)
+	{
+		Optional<Turma> turma =repoTur.findById(id);
+		return turma.orElseThrow(()->new ResourceNotFoundException(id));
+	}
+	
+	public Turma insert(Turma turma)
+	{
+		return repoTur.save(turma);
+	}
+	
+	public void delete(Long id)
+	{
+		try
+		{
+			repoTur.deleteById(id);
+		}
+		catch(ResourceNotFoundException e)
+		{
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e)
+		{
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	
+	public Turma update(Long id,Turma turma)
+	{
+		try
+		{
+			Turma turma1=repoTur.getReferenceById(id);
+			updateData(turma1,turma);
+			return repoTur.save(turma1);
+		}
+		catch(EntityNotFoundException e) 
+		{
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	
+	private void updateData(Turma turma1,Turma turma2)
+	{
+		turma1.setNome(turma2.getNome());
+		turma1.setEscola(turma2.getEscola());
+		turma1.setCapacidade(turma2.getCapacidade());
 	}
 }
